b7021d1024f6587fd20bb0cbfa35ec6bd760cf25
alamin <alaminhossen899@gmail.com>
2019-07-08 15:59:03 +0600

    hello

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -84,6 +84,7 @@
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Documentation</a>
+                    <a href="https://laravel.com/docs">Documentation</a>
                     <a href="https://laracasts.com">Laracasts</a>
                     <a href="https://laravel-news.com">News</a>
                     <a href="https://forge.laravel.com">Forge</a>
